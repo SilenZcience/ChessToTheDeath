3aba32f5839f13ab4eecbff7d60512efafacc051
Silas Kraume <silas.kraume1552@gmail.com>
2022-12-13 15:47:22 +0100

    requirements

--- chess_to_the_death/__init__.py
+++ chess_to_the_death/__init__.py
@@ -1,4 +1,4 @@
-"""Chess variant."""
+"""A simple chess variant where pieces have damage and health values."""
 
 __version__ = "0.0.1"
 __sysversion__ = '3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]'
