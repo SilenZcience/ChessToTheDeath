2981b741e823a8b382514d966c2ee1c3129651c6
Silas Kraume <silas.kraume1552@gmail.com>
2022-12-19 21:14:39 +0100

    catch keyboardinterrupt

--- chess_to_the_death/__main__.py
+++ chess_to_the_death/__main__.py
@@ -2,5 +2,8 @@ from chess_to_the_death.parser.argparser import ArgsHandler
 from chess_to_the_death.util.gui import mainGUI
 
 if __name__ == '__main__':
-    argsHandler = ArgsHandler(__file__)
-    mainGUI(argsHandler.getParams())
+    try:
+        argsHandler = ArgsHandler(__file__)
+        mainGUI(argsHandler.getParams())
+    except KeyboardInterrupt:
+        print("GoodBye!")
