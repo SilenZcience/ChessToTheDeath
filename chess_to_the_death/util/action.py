29fe81443a1c6d11a7ec2722947e7c337ed6aacd
Silas Kraume <silas.kraume1552@gmail.com>
2022-12-30 22:05:05 +0100

    action log type hints

--- chess_to_the_death/util/action.py
+++ chess_to_the_death/util/action.py
@@ -1,11 +1,23 @@
 import numpy as np
 
+class Action:
+    def __init__(self, board: np.ndarray, from_col: str, from_row: str, to_col: str, to_row: str, action: str) -> None:
+        self.board = board.copy()
+        self.from_col = from_col
+        self.from_row = from_row
+        self.to_col = to_col
+        self.to_row = to_row
+        self.action = action
+        
+    def __repr__(self) -> str:
+        return self.from_col + self.from_row + '-' + self.to_col + self.to_row + ' ' + self.action
+
 class ActionLog:
     actions = []
-    def add(self, board: np.ndarray, from_col: int, from_row: int, to_col: int, to_row: int, action: str):
+    def add(self, board: np.ndarray, from_col: int, from_row: int, to_col: int, to_row: int, action: str) -> None:
         self.actions.append(Action(board, from_col, from_row, to_col, to_row, action))
         
-    def get(self, index: int):
+    def get(self, index: int) -> Action:
         return self.actions[index]
     
     def getPrintActionString(self, actionLogIndex: int = -1, color: str = '32') -> str:
@@ -24,16 +36,4 @@ class ActionLog:
         
     def __repr__(self) -> str:
         return ''.join([self.getPrintActionString(i, '33') for i in range(len(self.actions))])
-
-class Action:
-    def __init__(self, board: np.ndarray, from_col: str, from_row: str, to_col: str, to_row: str, action: str) -> None:
-        self.board = board.copy()
-        self.from_col = from_col
-        self.from_row = from_row
-        self.to_col = to_col
-        self.to_row = to_row
-        self.action = action
-        
-    def __repr__(self) -> str:
-        return self.from_col + self.from_row + '-' + self.to_col + self.to_row + ' ' + self.action
         
