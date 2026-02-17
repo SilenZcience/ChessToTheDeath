44b67cbe00d47a9a1362287113925fa0f80fc58c
Silas Kraume <silas.kraume1552@gmail.com>
2026-02-17 15:46:40 +0100

    bump build version

--- chess_to_the_death/__init__.py
+++ chess_to_the_death/__init__.py
@@ -1,5 +1,5 @@
 """A simple chess variant where pieces have damage and health values."""
 
 __version__ = "0.1.3"
-__sysversion__ = '3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]'
+__sysversion__ = '3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
